38bd43e1cfb27ff597f21a3717ed54378c4fc9b4
Brigid Overton <brigid.overton@gmail.com>
2019-11-30 17:12:55 -0800

    added index.html and connected it to login.js

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+
 // /admin/add-product => GET
 router.get('/', function(req, res) {
-    res.send("Hello login World!"); 
+    //res.send("Hello login World!");
+    res.sendFile(process.cwd() + '/public/index.html');
 });
 
 router.post('/', function(req, res) {
-    
+
 });
 
 module.exports = router;
